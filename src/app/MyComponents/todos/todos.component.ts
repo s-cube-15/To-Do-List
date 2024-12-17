93f81c95d8ad8f870b5d77f8c69793cf62e0186c
s-cube-15 <2020.sudhanshu.sabale@ves.ac.in>
2024-12-17 22:29:50 +0530

    First Commit

---
+++ src/app/MyComponents/todos/todos.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css'],
+})
+export class TodosComponent {
+
+  todos: Todo[];
+  constructor() {
+    this.todos = [
+      { sno: 1, title: "This is title1", desc: "Descrip1", active: true },
+      { sno: 2, title: "This is title2", desc: "Descrip2", active: true },
+      { sno: 3, title: "This is title3", desc: "Descrip3", active: true },
+    ]
+  }
+
+  deleteTodo(todo: Todo) {
+    console.log(todo);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './MyComponents/todos/todos.component';
+import { TodoItemComponent } from './MyComponents/todo-item/todo-item.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    // TodoItemComponent,
+    // AddTodoComponent,
+    // AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
