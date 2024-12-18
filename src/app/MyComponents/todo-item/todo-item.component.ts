58d7ee825ba179328c8c73d4b7cd01b7f76c0d3e
s-cube-15 <2020.sudhanshu.sabale@ves.ac.in>
2024-12-18 12:27:40 +0530

    checkbox updates

--- src/app/MyComponents/todo-item/todo-item.component.ts
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -7,11 +7,16 @@ import { Todo } from 'src/app/Todo';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent {
+  @Input() i:number;
   @Input() todo: Todo;
   @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+  @Output() todoCheckbox: EventEmitter<Todo> = new EventEmitter();
   onClick(todo: Todo) {
     this.todoDelete.emit(todo);
     console.log("Onclick has been triggered")
   }
-
+onCheckboxClick(todo:any){
+  this.todoCheckbox.emit(todo);
+  
+}
 }
--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -9,23 +9,34 @@ import { Todo } from 'src/app/Todo';
   styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent {
-
+  localItem: any;
   todos: Todo[];
   constructor() {
-    this.todos = [
-      { sno: 1, title: "This is title1", desc: "Descrip1", active: true },
-      { sno: 2, title: "This is title2", desc: "Descrip2", active: true },
-      { sno: 3, title: "This is title3", desc: "Descrip3", active: true },
-    ]
-  }
+    this.localItem = localStorage.getItem("todos");
+    if (this.localItem == null){  
+        this.todos = []
+    }
+    else{   this.todos=JSON.parse(this.localItem);}
 
+
+  }
+  ngOnInit(): void {
+
+  }
   deleteTodo(todo: Todo) {
     console.log(todo);
-    const index=this.todos.indexOf(todo)
-    this.todos.splice(index,1);
+    const index = this.todos.indexOf(todo)
+    this.todos.splice(index, 1);
+    localStorage.setItem("todos", JSON.stringify(this.todos))
   }
   addTodo(todo: Todo) {
     console.log(todo);
     this.todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(this.todos))
+  }
+  toggleTodo(todo: Todo) {
+    const index = this.todos.indexOf(todo)
+    this.todos[index].active=!this.todos[index].active;
+    localStorage.setItem("todos", JSON.stringify(this.todos))
   }
 }
